decdd355bcc03fc40a1be00c733612dc0c691e55
hammadtech16 <hammadriaz170@gmail.com>
2025-09-22 12:54:18 -0700

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Icon } from '@iconify/react';
+import { useState } from "react";
+import "./App.css";
+import { Icon } from "@iconify/react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class='bg-gray-400 h-max'>
+        <div class='m-6' >
+          <div class='bg-cyan-700 w-max p-3'>
+            <h2>minecloud</h2>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <Icon icon="mdi:information" className="inline-block w-10 h-10 mr-1" />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
